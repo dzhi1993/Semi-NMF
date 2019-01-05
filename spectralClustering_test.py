3f2c9d92cf7bdcbaa01fa4b19943270d9aa1ff6d
Da Zhi <dzhi@uwo.ca>
2019-01-05 17:44:48 -0500

    Test spectral clustering method applied on 2D space data points.

--- spectralClustering_test.py
+++ spectralClustering_test.py
@@ -8,22 +8,12 @@ import matplotlib.pyplot as plt
 #X, y = datasets.make_blobs(n_samples=500, n_features=10, centers=5, cluster_std=[0.4, 0.3, 0.4, 0.3, 0.4], random_state=11)
 #print(X, X.shape)
 
-mat = spio.loadmat('groupData_sc1.mat')
-betaValueMatrix = mat['X_C']
+X = np.array([[1, 1], [2, 1], [1, 0],
+              [4, 7], [3, 5], [3, 6]])
+clustering = SpectralClustering(n_clusters=2,
+        assign_labels="discretize",
+        random_state=0).fit_predict(X)
+#print(clustering.labels_)
 
-print(betaValueMatrix.shape)
-
-# data = np.random.rand(13, 25)
-data = betaValueMatrix.transpose()
-
-y_pred = SpectralClustering(n_clusters=10, eigen_solver='amg').fit_predict(data)
-print(y_pred, "\n", y_pred.shape)
-filename = "spectralClusteringOutput_gamma=.mat"
-spio.savemat(filename, mdict={'data': y_pred})
-print("CH score with gamma= is: ", metrics.calinski_harabaz_score(data, y_pred))
-
-plt.imshow(y_pred, aspect='auto')
-plt.show()
-
-print(y_pred, y_pred.shape, "Done!")
-
+print(X.shape)
+print(clustering)
